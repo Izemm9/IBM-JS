2c48abfb44badea6bd65cb81a30e0ed02ea99ff6
Izemm9 <saidnbaja@outlook.fr>
2026-08-15 12:13:52 +0100

    Refactor user input variable declarations
    Removed redundant variable declarations for user input fields.

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,3 +1,11 @@
+ const username = document.getElementById('name').value;
+ const age = document.getElementById('age').value;
+ const email = document.getElementById('email').value;
+ const job = document.getElementById('job').value;
+ const designation = document.getElementById('designation').value;
+ const productType = document.getElementById('productType').value;
+ const feedback = document.getElementById('feedbackText').value;
+ const submitButton = document.getElementById('submitBtn');
 
 function submitFeedback() {
 
@@ -13,15 +21,6 @@ function submitFeedback() {
     document.getElementById('userInfo').style.display = 'block';
    
 }
-
- const username = document.getElementById('name').value;
- const age = document.getElementById('age').value;
- const email = document.getElementById('email').value;
- const job = document.getElementById('job').value;
- const designation = document.getElementById('designation').value;
- const productType = document.getElementById('productType').value;
- const feedback = document.getElementById('feedbackText').value;
- const submitButton = document.getElementById('submitBtn');
 
 submitButton.onclick = submitFeedback;
 
