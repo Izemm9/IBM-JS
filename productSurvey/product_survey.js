8216326d46cf0f8831ca08b49ead1b50ffe93dac
Izemm9 <saidnbaja@outlook.fr>
2026-08-15 12:22:59 +0100

    Refactor variable declarations into submitFeedback function
    Moved variable declarations inside the submitFeedback function for better scope management.

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,16 +1,14 @@
- const username = document.getElementById('name').value;
- const age = document.getElementById('age').value;
- const email = document.getElementById('email').value;
- const job = document.getElementById('job').value;
- const designation = document.getElementById('designation').value;
- const productType = document.getElementById('productType').value;
- const feedback = document.getElementById('feedbackText').value;
- const submitButton = document.getElementById('submitBtn');
-
 function submitFeedback() {
-
     alert('Thank you for your valuable feedback')
-
+        const username = document.getElementById('name').value;
+        const age = document.getElementById('age').value;
+        const email = document.getElementById('email').value;
+        const job = document.getElementById('job').value;
+        const designation = document.getElementById('designation').value;
+        const productType = document.getElementById('productType').value;
+        const feedback = document.getElementById('feedbackText').value;
+        const submitButton = document.getElementById('submitBtn');
+ 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
